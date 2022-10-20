6aaabd938549d5954435bccf5d68e0c172221c75
Kumara2mahe <mahendrakumara268@gmail.com>
2022-10-21 01:35:23 +0530

    Update: template-preview slider to more dynamic and efficient

--- Static/Assets/JavaScript/script.js
+++ Static/Assets/JavaScript/script.js
@@ -117,6 +117,7 @@ const sliderImageButton = (event) => {
             backgroundImage.classList.replace("slider-2", "slider-1")
         }
     }
+
     // Checking for the clicked button is 'button-2'
     else if (sliderButton.className == "button-2") {
 
@@ -154,109 +155,174 @@ if (sliderButton2 != null) {
 // Function for changing the image
 const sliderChangeImage = (event) => {
 
-    // Taking the specific element which is clicked 
-    let sliderArrow = event.target
+    // Gettings the element of clicked arrow and disabling it
+    let arrow = event.target
+    arrow.removeEventListener("click", sliderChangeImage)
 
-    // Getting the sibiling of the arrow buttons
-    let sibilings = nextButton.parentElement.childNodes
+    // Getting the element of each visible image
+    let visibleImages = thumbContainer.querySelectorAll("img.show")
+    let lenImages = visibleImages.length
 
-    // Getting the initial src value of each 'img' element
-    let lastImage = sibilings[9].attributes[0].value.split("_")
-    let thirdImage = sibilings[7].attributes[0].value.split("_")
-    let secondImage = sibilings[5].attributes[0].value.split("_")
-    let firstImage = sibilings[3].attributes[0].value.split("_")
+    let oppArrow, nextImage
+    setTimeout(() => {
 
-    // Getting image number from src attribute
-    let image4Num = parseInt(lastImage.pop().replace(".jpg", ""))
-    let image3Num = parseInt(thirdImage.pop().replace(".jpg", ""))
-    let image2Num = parseInt(secondImage.pop().replace(".jpg", ""))
-    let image1Num = parseInt(firstImage.pop().replace(".jpg", ""))
+        // Checking the clicked button is 'next'
+        if (arrow.classList[0] == "next" && arrow.classList[1] != "disabled-arrow") {
 
-    // Checking for the clicked button is 'next'
-    if (sliderArrow.className == "next") {
+            if (lenImages > 1) {
+                for (let i = 0; i < lenImages; i++) {
 
-        setTimeout(() => {
-
-            if (image4Num < 8) {
-
-                // First-Image
-                image1Num = image1Num + 1
-                sibilings[3].attributes[0].value = `${firstImage.join("_")}_0${image1Num}.jpg`
-
-                // Second-Image
-                image2Num = image2Num + 1
-                sibilings[5].attributes[0].value = `${secondImage.join("_")}_0${image2Num}.jpg`
-
-                // Third-Image
-                image3Num = image3Num + 1
-                sibilings[7].attributes[0].value = `${thirdImage.join("_")}_0${image3Num}.jpg`
-
-                // Fourth-Image
-                image4Num = image4Num + 1
-                sibilings[9].attributes[0].value = `${lastImage.join("_")}_0${image4Num}.jpg`
-
-                if (image4Num == 8) {
-                    sliderArrow.disabled = true
-                    sliderArrow.classList.toggle("disabled-arrow")
+                    if (i == 0) {
+                        visibleImages[i].style.width = 0
+                        visibleImages[i].classList.remove("show")
+                    }
+                    else if (i == lenImages - 1) {
+                        visibleImages[i].nextElementSibling.removeAttribute("style")
+                        visibleImages[i].nextElementSibling.classList.add("show")
+                    }
                 }
-                else {
-                    previousButton.disabled = false
-                    if (previousButton.classList[1] == "disabled-arrow") {
-                        previousButton.classList.remove("disabled-arrow")
+                // Setting the element of next image in list from left
+                nextImage = visibleImages[lenImages - 1].nextElementSibling.nextElementSibling
+            }
+            else {
+                visibleImages[0].style.width = 0
+                visibleImages[0].classList.remove("show")
+                visibleImages[0].nextElementSibling.removeAttribute("style")
+                visibleImages[0].nextElementSibling.classList.add("show")
+
+                // Setting the element of next image in list from left
+                nextImage = visibleImages[0].nextElementSibling.nextElementSibling
+            }
+            // Setting the element of non-clicked arrow
+            oppArrow = previousButton
+        }
+
+        // Checking the clicked button is 'previous'
+        else if (arrow.classList[0] == "previous" && arrow.classList[1] != "disabled-arrow") {
+
+            if (lenImages > 1) {
+                for (let i = 0; i < lenImages; i++) {
+
+                    if (i == 0) {
+                        visibleImages[i].previousElementSibling.removeAttribute("style")
+                        visibleImages[i].previousElementSibling.classList.add("show")
+                    }
+                    else if (i == lenImages - 1) {
+                        visibleImages[i].style.width = 0
+                        visibleImages[i].classList.remove("show")
                     }
                 }
             }
-        }, 300)
-    }
-    // Checking for the clicked button is 'previous'
-    else if (sliderArrow.className == "previous") {
+            else {
+                visibleImages[0].previousElementSibling.removeAttribute("style")
+                visibleImages[0].previousElementSibling.classList.add("show")
+                visibleImages[0].style.width = 0
+                visibleImages[0].classList.remove("show")
+            }
+            // Setting the element of next image in list from right and non-clicked arrow
+            nextImage = visibleImages[0].previousElementSibling.previousElementSibling
+            oppArrow = nextButton
+        }
+        // Enabling the another arrow if disabled
+        oppArrow.classList.remove("disabled-arrow")
+        oppArrow.addEventListener("click", sliderChangeImage)
 
-        setTimeout(() => {
+        // Enabling or Disabling the clicked arrow according to the presence of next image in list
+        if (nextImage == null) {
+            arrow.classList.add("disabled-arrow")
+        }
+        else {
+            arrow.classList.remove("disabled-arrow")
+            arrow.addEventListener("click", sliderChangeImage)
+        }
+    }, 150)
+}
 
-            if (image1Num > 1) {
+// Getting the element of next & previous button
+const previousButton = document.querySelector(".middle-imageslider .previous")
+const nextButton = document.querySelector(".middle-imageslider .next")
 
-                // First-Image
-                image1Num = image1Num - 1
-                sibilings[3].attributes[0].value = `${firstImage.join("_")}_0${image1Num}.jpg`
+// Getting the element of Thumbnail-Container in the Home-Page
+const thumbContainer = document.querySelector(".middle-imageslider .images-section .thumb-container")
+//
+if (thumbContainer != null) {
 
-                // Second-Image
-                image2Num = image2Num - 1
-                sibilings[5].attributes[0].value = `${secondImage.join("_")}_0${image2Num}.jpg`
+    // Disabling the arrow buttons
+    nextButton.classList.add("disabled-arrow")
+    previousButton.classList.add("disabled-arrow")
 
-                // Third-Image
-                image3Num = image3Num - 1
-                sibilings[7].attributes[0].value = `${thirdImage.join("_")}_0${image3Num}.jpg`
+    window.addEventListener("DOMContentLoaded", () => {
 
-                // Fourth-Image
-                image4Num = image4Num - 1
-                sibilings[9].attributes[0].value = `${lastImage.join("_")}_0${image4Num}.jpg`
+        // Getting all the 'img' elements inside the Thumbnail-Container
+        const thumbImages = Array.from(thumbContainer.querySelectorAll("img"))
+        const thumblen = thumbImages.length
 
-                if (image1Num == 1) {
-                    sliderArrow.disabled = true
-                    sliderArrow.classList.toggle("disabled-arrow")
+        // Getting the screen width & calculating the position of the image to split the Thumbnail-Container
+        const screenWidth = window.outerWidth
+        const splitThumb = Math.round(thumblen / 2)
+
+        let pos1, pos2
+        switch (true) {
+            case (screenWidth >= 1300 && screenWidth <= 1900):
+                pos1 = splitThumb - 2
+                pos2 = splitThumb + 2
+                break
+            case (screenWidth >= 900 && screenWidth <= 1299):
+                pos1 = splitThumb - 2
+                pos2 = splitThumb + 1
+                break
+            case (screenWidth >= 540 && screenWidth <= 899):
+                pos1 = splitThumb - 1
+                pos2 = splitThumb + 1
+                break
+            case (screenWidth <= 539):
+                pos1 = splitThumb - 1
+                pos2 = splitThumb
+                break
+            default:
+                pos1 = splitThumb - 3
+                pos2 = splitThumb + 3
+        }
+
+        // Splitting the 'img' elements inside the Thumbnail-Container into separate arrays
+        let leftImages = thumbImages.slice(0, pos1)
+        let visibleImages = thumbImages.slice(pos1, pos2)
+        let rightImages = thumbImages.slice(pos2)
+
+        for (let i = 0, j = 0, vlen = visibleImages.length - 1; i < thumblen; i++) {
+
+            if (visibleImages.includes(thumbImages[i])) {
+
+                // Making some of the images in Thumbnail-Container visible
+                thumbImages[i].classList.add("show")
+
+                if (vlen != 0 && j == 0 && thumbImages[i].previousElementSibling != null) {
+                    previousButton.addEventListener("click", sliderChangeImage)
+                    previousButton.classList.remove("disabled-arrow")
                 }
-                else {
-                    nextButton.disabled = false
-                    if (nextButton.classList[1] == "disabled-arrow") {
+                else if (vlen != 0 && j == vlen && thumbImages[i].nextElementSibling != null) {
+                    nextButton.addEventListener("click", sliderChangeImage)
+                    nextButton.classList.remove("disabled-arrow")
+                }
+                else if (vlen == 0) {
+                    if (thumbImages[i].previousElementSibling != null) {
+                        previousButton.addEventListener("click", sliderChangeImage)
+                        previousButton.classList.remove("disabled-arrow")
+                    }
+                    if (thumbImages[i].nextElementSibling != null) {
+                        nextButton.addEventListener("click", sliderChangeImage)
                         nextButton.classList.remove("disabled-arrow")
                     }
                 }
+                j++
             }
-        }, 300)
-    }
-}
-
-// Getting the element of previous button
-const previousButton = document.querySelector(".middle-imageslider .previous")
-//
-if (previousButton != null) {
-    previousButton.addEventListener("click", sliderChangeImage)
-}
-
-// Getting the element of next button
-const nextButton = document.querySelector(".middle-imageslider .next")
-//
-if (nextButton != null) {
-    nextButton.addEventListener("click", sliderChangeImage)
+            else if (leftImages.includes(thumbImages[i])) {
+                thumbImages[i].style.width = 0
+            }
+            else if (rightImages.includes(thumbImages[i])) {
+                thumbImages[i].style.width = 0
+            }
+        }
+    })
 }
 // ------------------------------------------------------------------------------------------------------------ //
